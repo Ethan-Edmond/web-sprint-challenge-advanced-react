ae74d7a5575f18901c8b2d7900d124e50fce9b07
Ethan-Edmond <eedmond@uccs.edu>
2021-06-04 10:43:51 -0600

    Added console log on error in PlantList

--- src/components/PlantList.js
+++ src/components/PlantList.js
@@ -16,11 +16,12 @@ export default class PlantList extends Component {
   componentDidMount() {
     axios.get('http://localhost:3333/plants')
       .then(res => {
-        console.log(res);
         this.setState({
           plants: res.data
         });
-      });
+
+      })
+      .catch(err => console.log(err));
   }
 
   render() {
