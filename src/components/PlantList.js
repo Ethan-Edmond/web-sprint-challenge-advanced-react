c93b8e179b56189650edf19c6e78ce761a03b37b
Ethan-Edmond <eedmond@uccs.edu>
2021-06-04 09:49:11 -0600

    Checked off readme and got PlantList Working. Peperomia Rosso image path is bugged

--- src/components/PlantList.js
+++ src/components/PlantList.js
@@ -2,6 +2,10 @@ import React, { Component } from "react";
 import axios from "axios";
 
 export default class PlantList extends Component {
+
+  state = {
+    plants: []
+  };
   // add state with a property called "plants" - initialize as an empty array
 
   // when the component mounts:
@@ -13,6 +17,9 @@ export default class PlantList extends Component {
     axios.get('http://localhost:3333/plants')
       .then(res => {
         console.log(res);
+        this.setState({
+          plants: res.data
+        });
       });
   }
 
