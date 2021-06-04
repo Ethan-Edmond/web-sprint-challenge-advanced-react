4f435dd30d8741a2c2e825b5f5ac48a8360b52e9
Ethan-Edmond <eedmond@uccs.edu>
2021-06-04 10:51:44 -0600

    Wrote CheckoutForm.test.js, needs to be broken

--- src/components/CheckoutForm.test.js
+++ src/components/CheckoutForm.test.js
@@ -1,9 +1,36 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import CheckoutForm from "./CheckoutForm";
 
 // Write up the two tests here and make sure they are testing what the title shows
 
-test("form header renders", () => {});
+test("form header renders", () => {
+  render(<CheckoutForm/>);
+});
 
-test("form shows success message on submit with form details", () => {});
+test("form shows success message on submit with form details", () => {
+  render(<CheckoutForm/>);
+  const firstName = screen.getByLabelText("First Name:");
+  userEvent.type(firstName, "John");
+
+  const lastName= screen.getByLabelText("Last Name:");
+  userEvent.type(lastName, "Doe");
+
+  const address = screen.getByLabelText("Address:");
+  userEvent.type(address, "1234 fake drive");
+
+  const city= screen.getByLabelText("City:");
+  userEvent.type(city, "Fake City");
+
+  const state = screen.getByLabelText("State:");
+  userEvent.type(state, "FL");
+
+  const zip = screen.getByLabelText("Zip:");
+  userEvent.type(zip, "12345");
+
+  const submit = screen.getByRole("button");
+  userEvent.click(submit);
+
+  const success = screen.getByTestId("successMessage");
+});
